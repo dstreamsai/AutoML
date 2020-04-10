2441be5c39f9590eb90c8200bdeeb379ac4e65fa
datastreamsai <andrey.rusov@dstreams.ai>
2020-04-10 19:30:48 +0300

    Moving to new DB images

--- MacOS/stop.sh
+++ MacOS/stop.sh
@@ -4,12 +4,6 @@ echo -e "\n=========="
 echo "Stoping containers. Please wait."
 echo -e "\n=========="
 
-ImagePath=$(basename $PWD | awk '{print tolower($0)}')
-DB_container=$(echo ${ImagePath}_alexdb_1)
-
-docker exec $DB_container sh -c '/etc/init.d/clickhouse-server stop'
-docker exec -u postgres $DB_container sh -c 'kill -INT `head -1 /var/lib/pgsql/9.6/data/postmaster.pid`'
-
 docker-compose stop
 
 echo -e "\n=========="
--- Windows/stop.ps1
+++ Windows/stop.ps1
@@ -2,11 +2,6 @@ echo "============="
 echo "Stopping containers. Please wait"
 echo "============="
 
-$ImagePath = (Get-Item $PWD).BaseName.ToLower()
-$DB_container = "$ImagePath`_alexdb_1"
-
-& "C:\Program Files\Docker Toolbox\docker" exec ${DB_container} sh -c '/etc/init.d/clickhouse-server stop'
-& "C:\Program Files\Docker Toolbox\docker" exec -u postgres ${DB_container} sh -c 'kill -INT `head -1 /var/lib/pgsql/9.6/data/postmaster.pid`'
 & "C:\Program Files\Docker Toolbox\docker-compose" stop
 
 echo "`r`n============="
